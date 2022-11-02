5afa69202172b1d4be27440b1151429da6569756
Koushik <55914356+koushik147@users.noreply.github.com>
2022-11-02 03:06:04 -0400

    Update ReverseLinkedList.py

--- LinkedList1/ReverseLinkedList.py
+++ LinkedList1/ReverseLinkedList.py
@@ -1,3 +1,5 @@
+#Time_Complexity: O(n) 
+#Space_Complexity : O(1)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None # initializing prev 
